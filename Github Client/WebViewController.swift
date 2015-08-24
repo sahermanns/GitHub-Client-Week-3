58bd4e69ec8ed62566c99f2c1ba67416177da100
Sarah Hermanns <sahermanns@gmail.com>
2015-08-23 17:47:46 -0700

    fixed some constraint issues in repo tableview

--- Github Client/WebViewController.swift
+++ Github Client/WebViewController.swift
@@ -16,7 +16,7 @@ class WebViewController: UIViewController {
 
       let webView = WKWebView(frame: view.frame)
       
-      let urlRequest = NSURLRequest(URL: NSURL(string: "http://google.com")!)
+      let urlRequest = NSURLRequest(URL: NSURL(string: "https://api.github.com/search/repositories")!)
       webView.loadRequest(urlRequest)
       
     }
@@ -25,16 +25,6 @@ class WebViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
